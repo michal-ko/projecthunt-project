0825b6a64ae65e1aaf3155e2ecda8251f3385568
michal <michal.koziatek@protonmail.com>
2020-05-10 12:48:02 +0100

    authentication model finished

--- accounts/views.py
+++ accounts/views.py
@@ -22,8 +22,18 @@ def signup(request):
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'login details incorrect'})
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return render(request, '')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
